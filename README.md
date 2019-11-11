b6c2a2d3cd92b852906663a7312e6c49bfa58b7e
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2019-11-11 17:47:19 +0100

    Solve leap and sum of multiples

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # F# Exercism
 
 Solutions for exercises on F# track on Exercism
+
+Code formatting:
+
+```powershell
+fantomas --noSpaceBeforeColon <file_name>
+```
--- leap/Leap.fs
+++ leap/Leap.fs
@@ -1,3 +1,4 @@
-﻿module Leap
+module Leap
 
-let leapYear (year: int): bool = (year % 4 = 0 && year % 100 <> 0) || year % 400 = 0
+let leapYear (year: int): bool =
+    (year % 4 = 0 && year % 100 <> 0) || year % 400 = 0
