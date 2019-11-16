be269e1da12cdce5df16b327502a63fba5e348d6
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2019-11-16 14:40:03 +0100

    Solve difference of square

--- difference-of-squares/DifferenceOfSquares.fs
+++ difference-of-squares/DifferenceOfSquares.fs
@@ -1,7 +1,9 @@
-﻿module DifferenceOfSquares
+module DifferenceOfSquares
 
-let squareOfSum (number: int): int = failwith "You need to implement this function."
+let squareOfSum (number: int): int = pown (List.sum [ 1..number ]) 2
 
-let sumOfSquares (number: int): int = failwith "You need to implement this function."
+let sumOfSquares (number: int): int =
+    List.sum [ for i in 1..number -> i * i ]
 
-let differenceOfSquares (number: int): int = failwith "You need to implement this function."
+let differenceOfSquares (number: int): int =
+    squareOfSum number - sumOfSquares number
